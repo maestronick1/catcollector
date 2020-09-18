52cff57191b7be82aebd266b00855be8dc237a24
maestronick1 <maestronicknrp@gmail.com>
2020-09-18 14:29:41 -0700

    fix modulenot found error

---
+++ main_aop/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MainAopConfig(AppConfig):
+    name = 'main_aop'
---
+++ main_aop/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse('<h1>Hello, World!</h1>')
+
+def about(request):
+    return HttpResponse('<h1> I am Taylor</h1>')
